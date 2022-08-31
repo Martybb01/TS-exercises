71a1bac4601c2c307406532a34c70d2852ce9030
Martib01 <martybb01@gmail.com>
2022-08-31 17:16:05 +0200

    End es-18

---
+++ Create intersection types/exercise-2-create-intersection-types-start.js
@@ -0,0 +1,22 @@
+"use strict";
+/**
+ * START: Follow the instructions below.
+ */
+exports.__esModule = true;
+var countryA = {
+    name: "Greece",
+    code: "GR",
+    languages: ["Greek"]
+};
+console.log(countryA);
+//
+// Use the type alias syntax: type CountryWithStatistics = type;
+//
+// Add a type annotation with the `CountryWithStatistics` type on
+// the variable `countryB`.
+var countryB = {
+    name: "China",
+    code: "CN",
+    population: 1412600000
+};
+console.log(countryB);
